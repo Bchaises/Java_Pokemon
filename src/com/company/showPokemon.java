90904372ff9d182e08dff99a821a41b0f8e18101
Bchaises <benjamin.chaises@gmail.com>
2020-12-01 11:59:04 +0100

    jout jframe

---
+++ src/com/company/showPokemon.java
@@ -0,0 +1,16 @@
+package com.company;
+
+import javax.swing.*;
+
+public class showPokemon extends JFrame{
+    private JPanel MainContent;
+    private JTextField textField2;
+    private JPanel duTexte;
+    private JLabel coucou;
+
+    showPokemon(){
+        this.setContentPane(MainContent);
+        this.pack();
+    }
+
+}
